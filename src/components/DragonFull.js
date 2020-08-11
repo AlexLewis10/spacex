ca8de762f46d812b8d9b2a4ae2a6a4afd8e3d639
Alex Lewis <alexlewis374@gmail.com>
2020-08-11 17:01:40 +0100

    Add props and return to DragonFull

--- src/components/DragonFull.js
+++ src/components/DragonFull.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
-function DragonFull () {
+function DragonFull ( {
+  name,
+  height,
+  payload,
+  heatShield,
+  fuel,
+  fuel2,
+  description,
+  wikipedia,
+  img2
+}
+) {
 
   return (
-    <div>
-
-    </div>
+      <div className='full-view'>
+        <p className='full-info'>{name}</p>
+        <p className='full-info'>Height: {height}m</p>
+        <p className='full-info'>Mass: {payload}kg</p>
+        <p className='full-info'>Heat shield resistant to {heatShield}°C</p>
+        <p className='full-info'>Fuel 1: {fuel}</p>
+        <p className='full-info'>Fuel 2: {fuel2}</p>
+        <p className='full-info'>Description: {description}</p>
+        <a className='full-info' href={wikipedia}>Wiki</a>
+        <img className='rocket-img' src={`${img2}`} alt={name}></img>
+      </div>
   )
 }
 
--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -15,20 +15,19 @@ function Dragons ( { dragons }) {
   const getFullDragonInfo = (name) => {
     setShowFullView(true)
     //Take a look at this method, refactor and remove unused stuff
-    dragonsInfo.filter((rocket) => {
-      if (rocket.rocket_name === name) {
+    dragonsInfo.filter((dragon) => {
+      if (dragon.name === name) {
         setFullView(<DragonFull 
-          // key={rocket.rocket_name}
-          // name={rocket.rocket_name}
-          // height={rocket.height.meters}
-          // mass={rocket.mass.kg}
-          // engines={rocket.engines.thrust_sea_level.kN}
-          // fuel={rocket.engines.propellant_1}
-          // fuel2={rocket.engines.propellant_2}
-          // wikipedia={rocket.wikipedia}
-          // description={rocket.description}
-          // img1={rocket.flickr_images[0]}
-          // img2={rocket.flickr_images[1]}
+          key={dragon.name}
+          name={dragon.name}
+          height={dragon.height_w_trunk.meters}
+          payload={dragon.launch_payload_mass.kg}
+          heatShield={dragon.heat_shield.temp_degrees}
+          fuel={dragon.thrusters[0].fuel_1}
+          fuel2={dragon.thrusters[0].fuel_2}
+          wikipedia={dragon.wikipedia}
+          description={dragon.description}
+          img2={dragon.flickr_images[1]}
         />)
       }
       return null
