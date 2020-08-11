670683617e83ec126c4e6545f3a45acaa2b403cb
Alex Lewis <alexlewis374@gmail.com>
2020-08-11 16:05:58 +0100

    Add displayDragons method

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -7,10 +7,25 @@ function Dragons ( { dragons }) {
   useEffect(() => {
   setDragonsInfo(JSON.parse(dragons))
   }, [dragons])
+
+  const displayDragons = () => {
+    return dragonsInfo ? dragonsInfo.map((dragon) => { 
+      return <Dragon 
+        key={dragon.dragon_name}
+        name={dragon.dragon_name}
+        height={dragon.height.meters}
+        payload={dragon.launch_payload_mass.kg}
+        img={dragon.flickr_images[0]}
+        // getFulldragonInfo={getFulldragonInfo}
+      /> 
+    }) : null
+  }
   
   return (
   <div>
-    
+    <div className='grid-container'>
+      {displayDragons()}
+    </div>
   </div>
   )
 }
