e6525c0905df4becee3b920a0ea85c781cb9a6a8
Alex Lewis <alexlewis374@gmail.com>
2020-08-11 14:15:45 +0100

    Add style for rocket.img

--- src/components/RocketFull.js
+++ src/components/RocketFull.js
@@ -10,22 +10,21 @@ function RocketFull ({
   fuel2, 
   wikipedia, 
   description, 
-  img1, 
   img2 
 }) {
 
   return(
     <div className='full-view'>
-      <p className='full-info'>Name: {name}</p>
+      <p className='full-info'>{name}</p>
       <p className='full-info'>Height: {height}m</p>
       <p className='full-info'>Mass: {mass}kg</p>
       <p className='full-info'>Engine: {engines}</p>
       <p className='full-info'>Fuel 1: {fuel}</p>
       <p className='full-info'>Fuel 2: {fuel2}</p>
-      <p className='full-info'>Description{description}</p>
-      <p className='full-info'>More info: {wikipedia}</p>
-      {/* <p className='full-info'>>{img1}</p>
-      <p className='full-info'>>{img2}</p> */}
+      <p className='full-info'>Description: {description}</p>
+      <img className='rocket-img' src={`${img2}`} alt={name}></img>
+      {/* <p className='full-info'>More info: {wikipedia}</p> */}
+      {/* <p className='full-info'>>{img2}</p> */}
     </div>
   )
 }
